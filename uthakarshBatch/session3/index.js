077b05766d95bf99c325bd360ffb69482fcf3884
nalina <naleenameshram85@gmail.com>
2025-11-10 22:31:30 +0530

    working on

--- uthakarshBatch/session3/index.js
+++ uthakarshBatch/session3/index.js
@@ -9,11 +9,17 @@ const HomeRouter = require("./routes/HomeRoutes");
 const ActivityRoutes = require("./routes/ActivityRoutes");
 const userData = require("./userData");
 const { Authoriza } = require("./Middleware/Authorization");
+const { default: mongoose } = require("mongoose");
 
 server.use("/", HomeRouter);
 // use allow the request like (get, post, patch, delete)
 
 server.use("/api/v1/users", ActivityRoutes);
+const databaseURL = "mongodb://localhost:27017";
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log("mongoDB Connected Successfully"))
+  .catch((error) => console.error());
 server.listen(PORT, () => {
   console.log(`server running on the PORT, ${PORT}`);
 });
